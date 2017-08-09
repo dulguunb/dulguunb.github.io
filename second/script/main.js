5821dd84a4e53dc758c7af3df2dc420da6f02b6d
dulguun <dulguunb@protonmail.com>
2017-08-09 19:08:58 +0900

    get schwifty 2

--- second/script/main.js
+++ second/script/main.js
@@ -14,7 +14,7 @@ var app = {
 				var promptValue = document.getElementById("prompt").value;
 					if(promptValue == 'Y' || promptValue == '' || promptValue == 'y'){
 						document.body.style.background = "url('assets/gif/cool.gif')";
-						document.getElementsByClassName("consoleoutput")[0].innerHTML = "<p> Initilizing init.py </p> <p> </p> HTTP request sent, awaiting response... 200 OK <p> Changing background image </p> <p> Getting schwifty </p>";
+						document.getElementsByClassName("consoleoutput")[0].innerHTML = "<p> Initializing init.py </p> <p> </p> HTTP request sent, awaiting response... 200 OK <p> Changing background image </p> <p> Getting schwifty </p>";
 					}
 					else if (promptValue=="exit()") {
 
