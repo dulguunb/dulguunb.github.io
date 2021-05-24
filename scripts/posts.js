68f8dfca9fa37e1e73f8c6e4c600995fba2b6296
Dulguun <dulguunb@protonmail.com>
2021-05-24 14:26:09 +0200

    Add a new post about isolation in docker and remove CNAME because the domain name is expired

--- scripts/posts.js
+++ scripts/posts.js
@@ -9,6 +9,11 @@ var posts = [
         name:  "Task Level Parrallelism",
         date:  "2021/03/20",
     },
+    {
+        topic: "Isolatation using Docker",
+        name:  "Isolatation using Docker",
+        date:  "2021/05/24",
+    },
     // {
     //     topic: "Concurrent Programming in Java",
     //     name:  "Introduction To Concurrency",
