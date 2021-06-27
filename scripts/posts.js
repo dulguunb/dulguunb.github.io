640bf2982554f4f02107518ae344a627aff1a2a8
Dulguun <dulguunb@protonmail.com>
2021-06-27 17:36:23 +0200

    Add TCP Hijacking post from Hacking Art of Exploitation by Jon Erickson

--- scripts/posts.js
+++ scripts/posts.js
@@ -14,6 +14,11 @@ var posts = [
         name:  "Isolatation using Docker",
         date:  "2021/05/24",
     },
+    {
+        topic: "Network Hacking",
+        name:  "TCP RST Hijack",
+        date:  "2021/06/27",
+    },
     // {
     //     topic: "Concurrent Programming in Java",
     //     name:  "Introduction To Concurrency",
